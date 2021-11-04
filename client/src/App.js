d193521661c8d21ba5950ecc6375e2c8c2c86bdd
nick0726 <75522130+nick0726@users.noreply.github.com>
2021-11-04 20:47:09 +0900

    Set Readct folder and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '../src/pages/landingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+         <div className="ivory-nav">
+         <img className="Phone-icon-image" alt='' src="image/Withyou_web_icon.svg"/>
+          <div className="login">Log-in</div>
+          <div className="bar">|</div>
+          <div className="join">Join</div>
+         </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function navBar() {
+    return (
+    <div className="nav">
+        <div className="ivory-nav">
+         <div className="login">log-in</div>
+         <div className="bar">|</div>         
+         <div className="join">Join</div>
+        </div>
+    </div>
+    );
+}
