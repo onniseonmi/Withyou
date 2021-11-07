0b5efc1c93e8e2b7dfee51bb7573724962978d6d
onniseonmi <soul012632@gmail.com>
2021-11-08 02:02:28 +0800

    Update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import EditPage from "./pages/EditPage";
-import LandingPage from "./pages/LandingPage";
-import Mypage from "./pages/Mypage";
-import Nav from "./components/Nav";
-import "./css/App.css";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import EditPage from './pages/EditPage';
+import LandingPage from './pages/LandingPage';
+import Mypage from './pages/Mypage';
+import Nav from './components/Nav';
+import './css/App.css';
 
-import Login from "./components/modals/auth/Login";
+import Login from './components/modals/auth/Login';
 
 export default function App() {
   const [isLogin, setLogin] = useState(false);
@@ -15,14 +15,14 @@ export default function App() {
     <Router>
       <Nav isLogin={isLogin} />
       <Switch>
-        <Route exact={true} path="/">
+        <Route exact={true} path='/'>
           <LandingPage />
         </Route>
-        <Route path="/editpage">
+        <Route path='/editpage'>
           <EditPage />
         </Route>
-        <Route path="/mypage">
-          <Mypage />
+        <Route path='/mypage'>
+          <Mypage accessToken={accessToken} />
         </Route>
       </Switch>
     </Router>
