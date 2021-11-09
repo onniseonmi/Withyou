a32a858024e94ea38d99fdb1ba8560ac6af6c867
onniseonmi <soul012632@gmail.com>
2021-11-09 22:17:09 +0800

    Update oauth github kakao

--- server/controllers/user/oauth/callback.js
+++ server/controllers/user/oauth/callback.js
@@ -1,27 +1,46 @@
-require("dotenv").config();
-const axios = require("axios");
+require('dotenv').config();
+const axios = require('axios');
 
 module.exports = (req, res) => {
   const { authorizationCode, type } = req.body;
-  let url = "";
-  let client_id = "";
-  let client_secret = "";
-  let state = "";
-  const redirect_uri = "http://localhost:3000";
-  if (type === "naver") {
+  let url = '';
+  let client_id = '';
+  let client_secret = '';
+  let state = '';
+  const redirect_uri = 'http://localhost:3000';
+  if (type === 'naver') {
     client_id = process.env.NAVER_CLIENT_ID;
     client_secret = process.env.NAVER_CLIENT_SECRET;
-    state = "RANDOM_STATE";
+    state = 'RANDOM_STATE';
     url = `https://nid.naver.com/oauth2.0/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&state=${state}&code=${authorizationCode}`;
-  } else if (type === "kakao") {
+    axios({ method: 'GET', url }).then((resp) => {
+      res.send({ access_token: resp.data.access_token });
+    });
+  } else if (type === 'kakao') {
     client_id = process.env.KAKAO_CLIENT_ID;
     client_secret = process.env.KAKAO_CLIENT_SECRET;
     url = `https://kauth.kakao.com/oauth/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${redirect_uri}`;
+    axios({ method: 'GET', url }).then((resp) => {
+      res.send({ access_token: resp.data.access_token });
+    });
+  } else if (type === 'github') {
+    const postData = {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code: authorizationCode,
+    };
+    axios({
+      method: 'POST',
+      url: 'https://github.com/login/oauth/access_token',
+      headers: {
+        accept: 'application/json',
+      },
+      data: postData,
+      withCredentials: true,
+    }).then((resp) => {
+      console.log('resp.data');
+      console.log(resp.data);
+      res.send({ access_token: resp.data.access_token });
+    });
   }
-  axios({
-    method: "GET",
-    url,
-  }).then((resp) => {
-    res.send({ access_token: resp.data.access_token });
-  });
 };
