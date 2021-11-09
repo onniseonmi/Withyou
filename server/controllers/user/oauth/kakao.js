437e4286a337ee3c0520d839991f946aef235dc6
Daehee Yoon <allendy11@gmail.com>
2021-11-09 16:53:22 +0900

    add server oauth

---
+++ server/controllers/user/oauth/kakao.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+module.exports = (req, res) => {
+  const authorization = req.headers["authorization"];
+  const token = authorization.split(" ")[1];
+  axios({
+    method: "GET",
+    url: "https://kapi.kakao.com/v2/user/me",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  }).then((resp) => {
+    const { profile, email } = resp.data.kakao_account;
+
+    res.send({
+      username: profile.nickname,
+      email: email,
+      image: profile.profile_image_url,
+    });
+  });
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { signin, signup, signout, deleteAccount } = require('../controllers');
+const {
+  signin,
+  signup,
+  signout,
+  deleteAccount,
+  callback,
+  kakao,
+  naver,
+} = require("../controllers");
 // const auth = require('../middelware/auth');
 
 /* GET users listing. */
-router.post('/signin', signin);
-router.post('/signup', signup);
-router.get('/signout', signout);
-router.get('/delete', deleteAccount);
-
+router.post("/signin", signin);
+router.post("/signup", signup);
+router.get("/signout", signout);
+router.get("/delete", deleteAccount);
+router.post("/callback", callback);
+router.get("/kakao", kakao);
+router.get("/naver", naver);
 module.exports = router;
