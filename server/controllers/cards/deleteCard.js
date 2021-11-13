50407f52a5d8c72248592174fde0e6f29ad766f7
onniseonmi <soul012632@gmail.com>
2021-11-14 00:35:47 +0800

    Updated card merged oauth again

---
+++ server/controllers/cards/deleteCard.js
@@ -0,0 +1,17 @@
+const { Card } = require('../../models');
+const auth = require('../../middelware/auth');
+
+module.exports = async (req, res) => {
+  const authHeader = auth(req);
+  if (!authHeader) {
+    res.status(404).send({ message: 'not authrized' });
+  }
+  const { id } = req.params;
+  await Card.destroy({
+    where: {
+      id,
+    },
+  }).then(() => {
+    res.sendStatus(204);
+  });
+};
