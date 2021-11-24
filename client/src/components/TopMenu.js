7ec867d45fa59a2a596e0c25461029ea01f340c8
duduki <pdwtop0105@naver.com>
2021-11-24 14:28:47 +0900

    [WIP..]

--- client/src/components/TopMenu.js
+++ client/src/components/TopMenu.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "../css/TopMenu.css";
-const TopMenu = () => {
+export default function TopMenu() {
+  async function startCapture(displayMediaOptions) {
+    let captureStream = null;
+
+    try {
+      captureStream = await navigator.mediaDevices.getDisplayMedia(
+        displayMediaOptions
+      );
+    } catch (err) {
+      console.error("Error: " + err);
+    }
+    return captureStream;
+  }
   return (
     <div id="top-menu">
       <div className="top-menu-box">
-        <div id="top-menu-save">저장하기</div>
+        <div id="top-menu-save" onClick={() => startCapture()}>
+          저장하기
+        </div>
       </div>
     </div>
   );
-};
-
-export default TopMenu;
+}
