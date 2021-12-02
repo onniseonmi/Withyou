dae86d5259f10bc5cc14179e177b1d37fb58d379
Daehee Yoon <allendy11@gmail.com>
2021-12-03 02:48:43 +0900

    fix landing

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -6,41 +6,29 @@ import { Link } from "react-router-dom";
 
 const LandingPage = ({ landingOn, setLandingOn }) => {
   return (
-    <div className="ladning-body">
-      <div className="landing">
-        <div className="video-box">
-          <video
-            id="landing-video"
-            autoplay="autoplay"
-            loop="Loop"
-            muted
-            volume="0"
-          >
-            <source src={landingVideo} type="video/mp4" />
-          </video>
-          <div className="landing-container">
-            <div className="landing-title">
-              <div>세상에 없던 단 하나뿐인</div>
-              <div>청첩장, 쉬운 디자인을 도와드립니다.</div>
-              <Link to="/editpage" className="phone-start">
-                <div
-                  className="main-get-started"
-                  onClick={() => setLandingOn(false)}
-                >
-                  시작하기
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
+    <div className="landing-container">
+      <div className="landing-box video-box">
+        <video
+          id="landing-video"
+          autoplay="autoplay"
+          loop="Loop"
+          muted
+          volume="0"
+        >
+          <source src={landingVideo} type="video/mp4" />
+        </video>
       </div>
-      <div
-        className="landing-footer"
-        style={{
-          backgroundColor: `${landingOn ? "transparent" : "#f2f0ec"}`,
-        }}
-      >
-        <Footer />
+      <div className="landing-box landing-title-box">
+        <div>세상에 없던 단 하나뿐인</div>
+        <div>청첩장, 쉬운 디자인을 도와드립니다.</div>
+        <Link to="/editpage" className="phone-start">
+          <div className="main-get-started" onClick={() => setLandingOn(false)}>
+            시작하기
+          </div>
+        </Link>
+      </div>
+      <div className="landing-footer">
+        <Footer landingOn={landingOn} />
       </div>
     </div>
   );
