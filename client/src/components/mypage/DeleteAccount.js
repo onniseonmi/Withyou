b7a201ff5457353878254c95479a6f3ab7e5f9fc
Daehee Yoon <allendy11@gmail.com>
2021-12-04 23:19:00 +0900

    fix css login on mobile

--- client/src/components/mypage/DeleteAccount.js
+++ client/src/components/mypage/DeleteAccount.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-const server_url_1 = 'http://localhost:4000';
+import React, { useEffect } from "react";
+import axios from "axios";
+const server_url_1 = "http://localhost:4000";
 const server_url_2 =
-  'http://ec2-3-24-168-238.ap-southeast-2.compute.amazonaws.com:4000';
+  "http://ec2-3-24-168-238.ap-southeast-2.compute.amazonaws.com:4000";
 
 export const DeleteAccount = () => {
-  const accessToken = sessionStorage.getItem('accessTokenSession');
-  console.log('accessToken');
+  const accessToken = sessionStorage.getItem("accessTokenSession");
+  console.log("accessToken");
   console.log(accessToken);
   const deleteAccount = async (accessToken) => {
-    console.log('삭제???');
+    console.log("삭제???");
     await axios.get(`${server_url_2}/user/delete`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -19,7 +19,7 @@ export const DeleteAccount = () => {
     // sessionStorage.removeItem('accessTokenSession');
   };
   return (
-    <div className='delete-account-container'>
+    <div className="delete-account-container">
       <button onClick={deleteAccount(accessToken)}>회원탈퇴</button>
     </div>
   );
