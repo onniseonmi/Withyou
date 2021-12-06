16bd2b55f275e17449632f6b54d2ce3716d41e2d
nick0726 <75522130+nick0726@users.noreply.github.com>
2021-12-07 00:08:42 +0900

    Added elements and background data images

--- client/src/images/elements/AllElements.js
+++ client/src/images/elements/AllElements.js
@@ -8,6 +8,25 @@ import sample_7 from "../../images/elements/sample 7.png";
 import sample_8 from "../../images/elements/sample 8.png";
 import leaf_deco from "../../images/elements/leaf_deco.svg";
 import rose_full from "../../images/elements/rose_full.svg";
+import element_1 from "../../images/elements/element_1.png";
+import element_2 from "../../images/elements/element_2.png";
+import element_3 from "../../images/elements/element_3.png";
+import element_4 from "../../images/elements/element_4.png";
+import element_5 from "../../images/elements/element_5.png";
+import element_6 from "../../images/elements/element_6.png";
+import element_7 from "../../images/elements/element_7.png";
+import element_8 from "../../images/elements/element_8.png";
+import element_9 from "../../images/elements/element_9.png";
+import element_10 from "../../images/elements/element_10.png";
+import element_11 from "../../images/elements/element_11.png";
+import element_13 from "../../images/elements/element_13.png";
+import element_14 from "../../images/elements/element_14.png";
+import element_15 from "../../images/elements/element_15.png";
+import element_16 from "../../images/elements/element_16.png";
+import element_17 from "../../images/elements/element_17.png";
+import element_18 from "../../images/elements/element_18.png";
+import element_20 from "../../images/elements/element_20.png";
+import element_21 from "../../images/elements/element_21.png";
 
 const samples = [
   { src: sample_1 },
@@ -20,12 +39,25 @@ const samples = [
   { src: sample_8 },
   { src: leaf_deco },
   { src: rose_full },
-  { src: leaf_deco },
-  { src: rose_full },
-  { src: leaf_deco },
-  { src: rose_full },
-  { src: leaf_deco },
-  { src: rose_full },
+  { src: element_1},
+  { src: element_2},
+  { src: element_3},
+  { src: element_4},
+  { src: element_5},
+  { src: element_6},
+  { src: element_7},
+  { src: element_8},
+  { src: element_9},
+  { src: element_10},
+  { src: element_11},
+  { src: element_13},
+  { src: element_14},
+  { src: element_15},
+  { src: element_16},
+  { src: element_17},
+  { src: element_18},
+  { src: element_20},
+  { src: element_21},
 ];
 
 export default samples;
