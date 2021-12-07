3f3293a00e38d27a11ef2e53a1a8f065066fa070
Daehee Yoon <allendy11@gmail.com>
2021-12-07 21:19:44 +0900

    login remove naver

--- client/src/components/login/Oauth.js
+++ client/src/components/login/Oauth.js
@@ -6,15 +6,15 @@ const client_url_2 =
   "http://withyou-final.s3-website.ap-northeast-2.amazonaws.com";
 
 const Oauth = () => {
-  const naverLogin = (e) => {
-    const client_id = "uFXw6VGES6r8XT7wm80Q";
-    const state = "RANDOM_STATE";
-    const api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${client_url_2}&state=${state}`;
-    // setUserInfo({ ...userInfo, type: "naver" });
-    // sessionStorage.setItem("userInfoSession", JSON.stringify(userInfo));
-    sessionStorage.setItem("loginType", "naver");
-    window.location.assign(api_url);
-  };
+  // const naverLogin = (e) => {
+  //   const client_id = "uFXw6VGES6r8XT7wm80Q";
+  //   const state = "RANDOM_STATE";
+  //   const api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&redirect_uri=${client_url_2}&state=${state}`;
+  //   // setUserInfo({ ...userInfo, type: "naver" });
+  //   // sessionStorage.setItem("userInfoSession", JSON.stringify(userInfo));
+  //   sessionStorage.setItem("loginType", "naver");
+  //   window.location.assign(api_url);
+  // };
 
   const kakaoLogin = (e) => {
     const client_id = "590eb89ea8da97055898d61a832ed657";
@@ -46,14 +46,14 @@ const Oauth = () => {
           깃허브 로그인
         </div>
       </div>
-      <div className="oauth-box">
+      {/* <div className="oauth-box">
         <div>
           <img id="naver-logo" src={naver} alt="naver"></img>
         </div>
         <div className="oauth-name button" onClick={naverLogin}>
           네이버 로그인
         </div>
-      </div>
+      </div> */}
       <div className="oauth-box">
         <div>
           <img id="kakao-logo" src={kakao} alt="kakao"></img>
