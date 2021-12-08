c2830c75aa0f292ce24055a3e6854a78593f32b8
duduki <pdwtop0105@naver.com>
2021-12-08 21:07:55 +0900

    FIX error Message

--- client/src/components/editpage/canvas/modals/ServerErrorMessage.js
+++ client/src/components/editpage/canvas/modals/ServerErrorMessage.js
@@ -1,24 +1,23 @@
 import React from "react";
 import "../../../../css/editpage/canvas/modals/Message.css";
-export default function ServerErrorMessage({
-  setIsServerError,
-  setIsMessage,
-}) {
+export default function ServerErrorMessage({ setIsServerError, setIsMessage }) {
   return (
-    <div id="message-modal">
-      죄송합니다. 오류가 발생했어요.
-      <br />
-      문제를 수정중이에요.
-      <div className="save-modal-container">
-        <button
-          id="close-message-modal"
-          onClick={() => {
-            setIsMessage(false);
-            setIsServerError(false);
-          }}
-        >
-          Close
-        </button>
+    <div id="message-contaniner">
+      <div id="message-modal">
+        죄송합니다. 오류가 발생했어요.
+        <br />
+        문제를 수정중이에요.
+        <div className="save-modal-container">
+          <button
+            id="close-message-modal"
+            onClick={() => {
+              setIsMessage(false);
+              setIsServerError(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
--- client/src/components/editpage/canvas/modals/SuccessMessage.js
+++ client/src/components/editpage/canvas/modals/SuccessMessage.js
@@ -2,20 +2,22 @@ import React from "react";
 import "../../../../css/editpage/canvas/modals/Message.css";
 export default function SuccessMessage({ setIsSuccessMessage, setIsMessage }) {
   return (
-    <div id="message-modal">
-      서버에 저장하였습니다.
-      <br /> MyPage에서 확인해주세요.
-      <br />
-      <div className="save-modal-container">
-        <button
-          id="close-message-modal"
-          onClick={() => {
-            setIsMessage(false);
-            setIsSuccessMessage(false);
-          }}
-        >
-          Close
-        </button>
+    <div id="message-contaniner">
+      <div id="message-modal">
+        서버에 저장하였습니다.
+        <br /> MyPage에서 확인해주세요.
+        <br />
+        <div className="save-modal-container">
+          <button
+            id="close-message-modal"
+            onClick={() => {
+              setIsMessage(false);
+              setIsSuccessMessage(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
